c60de1331f61ed809a555a7fca711f67aba5e393
えび🦐 <91860481+ebibibibibi@users.noreply.github.com>
2025-07-14 06:34:54 +0900

    Create Project Euler - Problem 1

---
+++ problem001.c
@@ -0,0 +1,24 @@
+/*
+ * Project Euler - Problem 1
+ * Source: http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%201
+ * 
+ * Problem 1 「3と5の倍数」  †
+ * 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり,
+ * これらの合計は 23 になる.
+ * 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+ * int a =10;
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main() {
+    printf("Project Euler - Problem 1\n");
+    
+    // TODO: Implement solution here
+    
+    return 0;
+}
